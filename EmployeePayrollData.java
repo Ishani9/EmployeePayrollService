91c8f55b8149ce931085657d2d0bc3df87313ccd
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-21 23:44:19 +0530

    Added: Ability to create payroll Service to Read and Write

--- EmployeePayrollData.java
+++ EmployeePayrollData.java
@@ -1,5 +1,19 @@
 package com.bl.assignment;
 
 public class EmployeePayrollData {
+	
+	public int id;
+	public String name;
+	public double salary;
+
+	public EmployeePayrollData(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String toString() {
+		return "ID = " + id + ", NAME = " + name  + ", SALARY = " + salary;
+	}
 
 }
